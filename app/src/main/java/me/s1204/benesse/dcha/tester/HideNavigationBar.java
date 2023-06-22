df200c7e903103aabc17431c8177994237be6a7f
Syuugo <pub@s1204.me>
2023-06-22 18:06:23 +0900

    動作検証

--- app/src/main/java/me/s1204/benesse/dcha/tester/HideNavigationBar.java
+++ app/src/main/java/me/s1204/benesse/dcha/tester/HideNavigationBar.java
@@ -1,7 +1,11 @@
 package me.s1204.benesse.dcha.tester;
 
 import android.app.Activity;
+import android.content.ComponentName;
+import android.content.Intent;
+import android.content.ServiceConnection;
 import android.os.Bundle;
+import android.os.IBinder;
 import android.os.RemoteException;
 import android.provider.Settings;
 
@@ -10,16 +14,23 @@ import jp.co.benesse.dcha.dchaservice.IDchaService;
 public class HideNavigationBar extends Activity {
     IDchaService mDchaService;
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        try {
-            if (Settings.System.getInt(getContentResolver(), "hide_navigation_bar") == 1) {
-                mDchaService.hideNavigationBar(false);
-            } else {
-                mDchaService.hideNavigationBar(true);
+        bindService(new Intent("jp.co.benesse.dcha.dchaservice.DchaService").setPackage("jp.co.benesse.dcha.dchaservice"), new ServiceConnection() {
+            @Override
+            public void onServiceConnected(ComponentName componentName, IBinder iBinder) {
+                mDchaService = IDchaService.Stub.asInterface(iBinder);
+                try {
+                    mDchaService.hideNavigationBar(Settings.System.getInt(getContentResolver(), "hide_navigation_bar") != 1);
+                } catch (RemoteException | Settings.SettingNotFoundException ignored) {
+                }
+                unbindService(this);
             }
-        } catch (Settings.SettingNotFoundException | RemoteException e) {
-            throw new RuntimeException(e);
-        }
+            @Override
+            public void onServiceDisconnected(ComponentName componentName) {
+                unbindService(this);
+            }
+        }, 1);
+        finishAndRemoveTask();        
     }
 }
